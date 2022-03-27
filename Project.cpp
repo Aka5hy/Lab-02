b27367ba6d5f4b78404038a0ef115c65000df9e7
MagovDI <MagovDI@mpei.ru>
2022-03-27 16:55:02 +0300

    code: Ввел разность

--- Project.cpp
+++ Project.cpp
@@ -5,7 +5,8 @@ int main()
     std::cout << "Enter A and B: ";
     int a, b;
     std:: cin >> a >> b;
-    std::cout << "A + B = " << a + b << std :: endl;
+    std::cout << "A + B = " << a + b << std::endl
+              << "A + B = " << a + b << std::endl;
     return 0;
 }
 
